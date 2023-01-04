444aa537388b8abc108205a66374cf05f18d41b2
Eyal <eyal@blockfence.io>
2023-01-04 15:48:34 +0200

    CR Fixes

--- src/pages/Blocked/index.tsx
+++ src/pages/Blocked/index.tsx
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import getUrlParams from '../../helpers/getURLParams';
 
-import { Button } from '../../components/Button';
 import '../../shared/reset.css';
 import * as Styled from './index.styled';
 
 function Panel() {
-    const [counter, setCounter] = useState(0);
     const urlParams = getUrlParams();
     console.log(urlParams);
 
@@ -18,15 +16,6 @@ function Panel() {
             <h2>Whoops... This website looks suspicious</h2>
 
             <h3>{urlParams.url}</h3>
-
-            <p>In the meanwhilte, I can count your clicks... You have clicked {counter} times</p>
-            <Button
-                onClick={() => {
-                    setCounter(counter + 1);
-                }}
-            >
-                Click
-            </Button>
         </Styled.Container>
     );
 }
--- src/scripts/background/index.ts
+++ src/scripts/background/index.ts
@@ -3,6 +3,8 @@ import isBlockedURL from '../../helpers/isBlockedURL';
 
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
     const { tabId, url, frameId } = details;
+
+    // (frameId !== 0) only on navigation in internal iframes (currently we won't track them)
     if (!url || !url.startsWith('http') || frameId !== 0) {
         return;
     }
