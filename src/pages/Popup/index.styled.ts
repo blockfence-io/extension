8a48773b9d0c03931edac4e5cbda8707ceced0d4
Eyal <eyal@blockfence.io>
2023-01-18 16:11:00 +0200

    Added help message

--- src/pages/Popup/index.styled.ts
+++ src/pages/Popup/index.styled.ts
@@ -46,3 +46,18 @@ export const Form = styled.form`
     display: flex;
     gap: 10px;
 `;
+
+export const Help = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    font-size: 1rem;
+    width: 60%;
+    align-self: center;
+    font-weight: 300;
+    opacity: 0.7;
+    user-select: none;
+`;
