6be2bbd1735e9ac59e1c60876c9058208b9e74c3
Eyal <eyal@blockfence.io>
2023-02-20 14:33:37 +0200

    Initial working session backup

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { useAsyncCallback } from 'react-async-hook';
+import { useAsyncCallback, AsyncState } from 'react-async-hook';
 
 import * as Layout from '../../components/Layout.styles';
 import { SearchBar } from '../../components/SearchBar';
@@ -9,12 +9,11 @@ import { ErrorMessage, LoadingMessage } from '../../components/PageMessages';
 import { ContentDecoder } from '../../components/ContentDecoder';
 
 import { fetchAnalyze, fetchDescription } from '../../shared/api';
+import { logPageView, logSearchClick } from '../../shared/logs';
 import * as Styled from './index.styled';
 
 import '../../shared/reset.css';
 import '../../shared/font.css';
-
-import { logPageView, logSearchClick } from '../../shared/logs';
 
 function Panel() {
     const [to, setTo] = useState('');
@@ -22,9 +21,47 @@ function Panel() {
     const descriptionResult = useAsyncCallback(async (chainId, to) => fetchDescription(chainId, to));
     const analyzeResult = useAsyncCallback(async (chainId, to) => fetchAnalyze(chainId, to));
 
+    function saveSessionState() {
+        chrome.storage.local.set({
+            latestState: {
+                to,
+                chainId,
+                descriptionResult: descriptionResult.result,
+                analyzeResult: analyzeResult.result,
+            },
+        });
+    }
+
+    async function restoreSessionState() {
+        const storage = await chrome.storage.local.get('latestState');
+        const state = storage.latestState;
+        if (state) {
+            setTo(state.to);
+            setChainId(state.chainId);
+            console.log('Resotre: ', state.analyzeResult);
+            descriptionResult.set({
+                result: state.descriptionResult,
+                status: 'not-requested',
+                loading: false,
+                error: undefined,
+            });
+            analyzeResult.set({
+                result: state.analyzeResult,
+                status: 'not-requested',
+                loading: false,
+                error: undefined,
+            });
+        }
+    }
+
     useEffect(() => {
         logPageView('Popup');
+        restoreSessionState();
     }, []);
+
+    useEffect(() => {
+        saveSessionState();
+    }, [descriptionResult, analyzeResult]);
 
     async function handleClick(chainId: string, to: string) {
         setChainId(chainId);
@@ -35,6 +72,8 @@ function Panel() {
     }
     const chatError =
         "GPT-3's experiencing some technical difficulties, but don't worry, our team's on it. In the meantime, give it another try or holla at us if you need a hand.";
+
+    console.log('## Res: ', analyzeResult.result);
 
     return (
         <Layout.Container>
