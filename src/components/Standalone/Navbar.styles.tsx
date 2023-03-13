688b1627e791b821836aeb97787a900ab10fc207
Eyal <eyal@blockfence.io>
2023-03-13 11:38:14 +0200

    Added add to chrome

--- src/components/Standalone/Navbar.styles.tsx
+++ src/components/Standalone/Navbar.styles.tsx
@@ -42,16 +42,44 @@ export const Company = styled.div`
 `;
 
 export const Navigation = styled.div`
-    & a {
-    }
+    display: flex;
+    align-items: center;
+    gap: 20px;
 
     & svg {
         fill: white;
-        margin-left: 20px;
         opacity: 1;
 
         &:hover {
             opacity: 0.8;
+        }
+    }
+`;
+
+export const ExtensionsLink = styled.a`
+    color: white;
+    background: #8f00ff;
+    border-radius: 50px;
+
+    font-size: 16px;
+    font-weight: bold;
+
+    padding: 10px 20px;
+
+    display: flex;
+    align-items: center;
+    gap: 12px;
+
+    &:hover {
+        text-decoration: none;
+        background: #a93cff;
+    }
+
+    & svg {
+        height: 30px;
+
+        &:hover {
+            opacity: 1;
         }
     }
 `;
--- src/components/Standalone/Navbar.tsx
+++ src/components/Standalone/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Logo from '../../assets/logo-white.svg';
+import ChromeLogo from '../../assets/icons/chrome.svg';
 
 import { UilGithub } from '@iconscout/react-unicons';
 import { UilTwitter } from '@iconscout/react-unicons';
@@ -18,6 +19,14 @@ export function Navbar() {
                 <Styled.Company>Blockfence</Styled.Company>
             </Styled.Logo>
             <Styled.Navigation>
+                <Styled.ExtensionsLink
+                    href='https://chrome.google.com/webstore/detail/blockfence/cpgbcelefhmacblaocimfilfnchkghba'
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    <ChromeLogo />
+                    Add to chrome
+                </Styled.ExtensionsLink>
                 <a href='https://github.com/blockfence-io/extension' target='_blank' rel='noreferrer'>
                     <UilGithub size={ICON_SIZE} />
                 </a>
@@ -27,12 +36,6 @@ export function Navbar() {
                 <a href='https://twitter.com/blockfence_io' target='_blank' rel='noreferrer'>
                     <UilTwitter size={ICON_SIZE} />
                 </a>
-                {/* <a href='https://www.reddit.com/r/blockfence/' target='_blank' rel='noreferrer'>
-                    <UilRedditAlienAlt size={ICON_SIZE} />
-                </a> */}
-                {/* <a href='https://www.blockfence.io' target='_blank' rel='noreferrer'>
-                    <UilHome size={ICON_SIZE} />
-                </a> */}
             </Styled.Navigation>
         </Styled.Container>
     );
