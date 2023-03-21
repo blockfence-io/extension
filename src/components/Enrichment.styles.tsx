13b1e3bd3d303045bef5e9d3c34ecb1434295f5f
tal-blockfence <tal@blockfence.io>
2023-03-21 15:04:23 +0200

    Update Design

--- src/components/Enrichment.styles.tsx
+++ src/components/Enrichment.styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import * as theme from '../shared/theme';
 
 export const Title = styled.div`
     font-weight: bold;
@@ -7,13 +8,29 @@ export const Title = styled.div`
     font-size: 12px;
 `;
 
-export const Copyrights = styled.div`
-    color: black;
-    font-size: 10px;
-    margin-top: 14px;
+export const PoweredBy = styled.div`
+    color: ${theme.riskText.NONE};
+    font-size: 16px;
+    margin-top: 4px;
+    font-weight: bold;
+    align-items: center;
+    display: flex;
+    text-align: center;
+    justify-content: center;
 
     & svg {
         vertical-align: top;
         margin-right: 5px;
+    }
+`;
+
+export const ExtensionsLink = styled.a`
+    padding: 5px
+
+    display: flex;
+
+    &:hover {
+        text-decoration: none;
+        background: #a93cff;
     }
 `;
