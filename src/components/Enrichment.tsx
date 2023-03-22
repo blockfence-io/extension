d984f617e7e6c6bfe4916b972a2d5c80b8443e87
Eyal <eyal@blockfence.io>
2023-03-22 18:22:59 +0200

    Updated styles

--- src/components/Enrichment.tsx
+++ src/components/Enrichment.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { DataEnrichment, Risk as RiskType } from '../types/api';
+import { DataEnrichment } from '../types/api';
 import { Collapsable } from './UI/Collapsable';
 import RadarIcon from '../assets/icons/radar-icon.svg';
 
 import * as Styled from './Enrichment.styles';
-import { Icon } from './Risk.styles';
 
 interface EnrichmentProps {
     dataEnrichment: DataEnrichment;
@@ -13,22 +12,28 @@ interface EnrichmentProps {
 
 export function Enrichment({ dataEnrichment, defaultState = false }: EnrichmentProps) {
     const logo = dataEnrichment.dapp_logo ? <img src={dataEnrichment.dapp_logo} width='24' /> : <RadarIcon />;
+
     return (
         <Collapsable title={dataEnrichment.title} icon={logo} defaultState={defaultState}>
-            <Styled.ExtensionsLink href={dataEnrichment.link} target='_blank' rel='noreferrer'>
-                {dataEnrichment.powered_by && (
-                    <Styled.PoweredBy>
-                        <img src={dataEnrichment.icon} width='24' />
-                        {dataEnrichment.powered_by}
-                    </Styled.PoweredBy>
-                )}
-            </Styled.ExtensionsLink>
+            {dataEnrichment.link && (
+                <Styled.ExtensionsLink href={dataEnrichment.link} target='_blank' rel='noreferrer'>
+                    <img src={dataEnrichment.icon} width='24' /> Read more in {dataEnrichment.powered_by}
+                </Styled.ExtensionsLink>
+            )}
+
             {dataEnrichment.stats.map((stat, id) => (
                 <>
                     <Styled.Title key={id}>{stat.name}</Styled.Title>
                     {stat.value}
                 </>
             ))}
+
+            {dataEnrichment.powered_by && (
+                <Styled.PoweredBy>
+                    <img src={dataEnrichment.icon} width='24' />
+                    Powered by {dataEnrichment.powered_by}
+                </Styled.PoweredBy>
+            )}
         </Collapsable>
     );
 }
