a65ea55bd53f52c2d37ad1677ce247847565a5b7
gal-blockfence <130892418+gal-blockfence@users.noreply.github.com>
2023-05-17 11:13:47 +0300

    GEN-435-Fix-extension-to-not-show-unexpected-USD-values (#63)
    * Not showing USD for unexpected values
    
    * Removed ~ as it looks too much like -

--- src/components/Simulation.tsx
+++ src/components/Simulation.tsx
@@ -15,6 +15,10 @@ const getAmountString = (amount?: number, symbol?: string) => {
     return amount ? `${getFormattedNumber(amount)} ${symbol}` : "0";
 }
 
+const shouldShowUSD = (amount?: number) => {
+    return amount && amount > 0
+}
+
 export function Simulation({ simulation, defaultState = false }: SimulationProps) {
     return (
         
@@ -25,7 +29,9 @@ export function Simulation({ simulation, defaultState = false }: SimulationProps
                     <Styled.CenteredIconWithText>
                         {getAmountString(simulation?.outgoing_transaction.amount, simulation?.outgoing_transaction.symbol)}
                         {simulation?.outgoing_transaction.logo && <img src={simulation.outgoing_transaction.logo} width='18' />}
-                        {simulation?.outgoing_transaction.usd && <div>~${getFormattedNumber(simulation?.outgoing_transaction.usd)}</div>}
+                        {shouldShowUSD(simulation?.outgoing_transaction.usd) == true &&
+                            <div>${getFormattedNumber(simulation?.outgoing_transaction.usd)}</div>
+                        }
                     </Styled.CenteredIconWithText>
                     
                 </Styled.SectionContainer>
@@ -34,14 +40,16 @@ export function Simulation({ simulation, defaultState = false }: SimulationProps
                     <Styled.CenteredIconWithText>
                         {getAmountString(simulation?.incoming_transaction.amount, simulation?.incoming_transaction.symbol)}
                         {simulation?.incoming_transaction.logo && <img src={simulation.incoming_transaction.logo} width='18' />}
-                        {simulation?.incoming_transaction.usd && <div>~${getFormattedNumber(simulation?.incoming_transaction.usd)}</div>}
+                        {shouldShowUSD(simulation?.incoming_transaction.usd) == true &&
+                            <div>${getFormattedNumber(simulation?.incoming_transaction.usd)}</div>
+                        }
                     </Styled.CenteredIconWithText>
                     
                 </Styled.SectionContainer>
             </Styled.Container>
             {/* <Styled.CenteredIconWithText>
                 <div>Gas used: {getAmountString(simulation?.gas_used, simulation?.gas_symbol)}</div>
-                {simulation?.gas_usd && <div>~${getFormattedNumber(simulation?.gas_usd)}</div>}
+                {simulation?.gas_usd && <div>${getFormattedNumber(simulation?.gas_usd)}</div>}
             </Styled.CenteredIconWithText> */}
         </Collapsable>
     );
