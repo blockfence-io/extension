0fe36bea2724f04920a3f1ed4f4a4948315eadcb
tal-blockfence <tal@blockfence.io>
2023-06-20 15:07:42 +0300

    liner warning

--- src/components/UI/InfoTooltip.tsx
+++ src/components/UI/InfoTooltip.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Tooltip } from 'react-tooltip';
-import * as theme from '../../shared/theme';
 
 import * as Styled from './InfoTooltip.styles';
 
@@ -10,13 +9,13 @@ export interface InfoIconProps {
 }
 
 export const InfoTooltip: React.FC<InfoIconProps> = ({ name, tooltipText }) => {
-    const children = <Styled.Content>{tooltipText}</Styled.Content>;
-
     const id = 'name-' + name;
     return (
         <>
             <Styled.InfoIcon data-tooltip-id={id}>i</Styled.InfoIcon>
-            <Tooltip id={id} children={children} />
+            <Tooltip id={id}>
+                <Styled.Content>{tooltipText}</Styled.Content>
+            </Tooltip>
         </>
     );
 };
