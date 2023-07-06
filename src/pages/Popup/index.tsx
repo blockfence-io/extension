a2a78b00f8ae753a6dcfde6bae1be92aac935ac1
Eyal <eyal@blockfence.io>
2023-07-06 14:50:49 +0300

    Added overlay scroller support for Popup

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -6,13 +6,19 @@ import { logPageView } from '../../shared/logs';
 
 import '../../shared/reset.css';
 import '../../shared/font.css';
+import 'overlayscrollbars/overlayscrollbars.css';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 
 function Popup() {
     useEffect(() => {
         logPageView('Popup');
     }, []);
 
-    return <PopupPanel />;
+    return (
+        <OverlayScrollbarsComponent defer style={{ width: '376px', height: '600px' }}>
+            <PopupPanel />
+        </OverlayScrollbarsComponent>
+    );
 }
 
 const container = window.document.querySelector('#app-container');
